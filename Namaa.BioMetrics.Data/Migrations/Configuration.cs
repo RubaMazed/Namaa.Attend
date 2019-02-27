259b2fcc281dd9f55b0cfc8a286a7bccac1c7f4e
Ruba Mazed <ruba.m.88@gmail.com>
2019-02-27 13:07:52 +0200

    Add Context Class

---
+++ Namaa.BioMetrics.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Namaa.BioMetrics.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Namaa.BioMetrics.Data.NamaaBioMetricsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Namaa.BioMetrics.Data.NamaaBioMetricsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Namaa.BioMetrics.Data/NamaaBioMetricsContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Namaa.BioMetrics.Model;
+namespace Namaa.BioMetrics.Data
+{
+    public class NamaaBioMetricsContext : DbContext
+    {
+        public NamaaBioMetricsContext() : base("name=LocalConnection")
+        {
+
+        }
+        public DbSet<CommunityCenter> CommunityCenters { get; set; }
+        public DbSet<UserInfo> UserInfos { get; set; }
+        public DbSet<LogDataInfo> LogDataInfos { get; set; }
+    }
+}
